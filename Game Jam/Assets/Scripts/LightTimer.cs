0d12450ab196a38afa0544601e70dd4a68d84bde
Jason Mittertreiner <jason.mittertreiner@gmail.com>
2015-05-30 21:27:02 -0400

    Updated Level

---
+++ Game Jam/Assets/Scripts/LightTimer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightTimer : MonoBehaviour {
+
+	public Light light;
+	// Use this for initialization
+	void Start () {
+		light = this.GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Light.intensity = 2;
+	}
+}
