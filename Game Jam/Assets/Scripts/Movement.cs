cecc5953d5e4e76188e3929cbee90c92df038e09
Neil Redman <nredman@uwaterloo.ca>
2015-05-31 17:38:51 -0400

    Some character changes for aiming

--- Game Jam/Assets/Scripts/Movement.cs
+++ Game Jam/Assets/Scripts/Movement.cs
@@ -15,45 +15,36 @@ public class Movement : MonoBehaviour {
 	void Update () {
 		float horizontalAxis = Input.GetAxis ("Horizontal"+PlayerString);
 		float verticalAxis = Input.GetAxis ("Vertical"+PlayerString);
-
-        if (Mathf.Abs(verticalAxis) > Mathf.Abs(horizontalAxis))
-        {
-            if (verticalAxis > 0 )
-            {
-                animator.SetInteger("direction", 2);
-                animator.SetBool("idle", false);
-            }
-            else if (verticalAxis < 0)
-            {
-                animator.SetInteger("direction", 0);
-                animator.SetBool("idle", false);
-            }
-        }
-        else
-        {
-            if (horizontalAxis > 0)
-            {
-                animator.SetInteger("direction", 1);
-                animator.SetBool("idle", false);
-            }
-            else if (horizontalAxis < 0)
-            {
-                animator.SetInteger("direction", 3);
-                animator.SetBool("idle", false);
-            }
-            else
-            {
-                animator.SetBool("idle", true);
-            }
-        }
+		if (!Input.GetButton ("Fire" + PlayerString)) {
+			if (Mathf.Abs (verticalAxis) > Mathf.Abs (horizontalAxis)) {
+				if (verticalAxis > 0) {
+					animator.SetInteger ("direction", 2);
+					animator.SetBool ("idle", false);
+				} else if (verticalAxis < 0) {
+					animator.SetInteger ("direction", 0);
+					animator.SetBool ("idle", false);
+				}
+			} else {
+				if (horizontalAxis > 0) {
+					animator.SetInteger ("direction", 1);
+					animator.SetBool ("idle", false);
+				} else if (horizontalAxis < 0) {
+					animator.SetInteger ("direction", 3);
+					animator.SetBool ("idle", false);
+				} else {
+					animator.SetBool ("idle", true);
+				}
+			}
         
         
 
-        Vector3 toMove = new Vector3(horizontalAxis,verticalAxis);
-		toMove.Normalize ();
+			Vector3 toMove = new Vector3 (horizontalAxis, verticalAxis);
+			toMove.Normalize ();
 		 
-		GetComponent<CharacterController>().Move(toMove*speed*Time.deltaTime);
-		
+			GetComponent<CharacterController> ().Move (toMove * speed * Time.deltaTime);
+		} else {
+			animator.SetBool ("idle", true);
+		}
 	}
 
 	void Die(int a){
