d70afe7b5631ac72c0bb7d38337f73c9994eb3b7
Ian Holstead <ian.holstead@gmail.com>
2015-05-31 15:07:15 -0400

    Movement working

--- Game Jam/Assets/Scripts/Movement.cs
+++ Game Jam/Assets/Scripts/Movement.cs
@@ -16,30 +16,38 @@ public class Movement : MonoBehaviour {
 		float horizontalAxis = Input.GetAxis ("Horizontal"+PlayerString);
 		float verticalAxis = Input.GetAxis ("Vertical"+PlayerString);
 
-        if (verticalAxis > 0)
+        if (Mathf.Abs(verticalAxis) > Mathf.Abs(horizontalAxis))
         {
-            animator.SetInteger("direction", 2);
-            animator.SetBool("idle", false);
-        }
-        else if (verticalAxis < 0)
-        {
-            animator.SetInteger("direction", 0);
-            animator.SetBool("idle", false);
-        }
-        else if (horizontalAxis > 0)
-        {
-            animator.SetInteger("direction", 1);
-            animator.SetBool("idle", false);
-        }
-        else if (horizontalAxis < 0)
-        {
-            animator.SetInteger("direction", 3);
-            animator.SetBool("idle", false);
+            if (verticalAxis > 0 )
+            {
+                animator.SetInteger("direction", 2);
+                animator.SetBool("idle", false);
+            }
+            else if (verticalAxis < 0)
+            {
+                animator.SetInteger("direction", 0);
+                animator.SetBool("idle", false);
+            }
         }
         else
         {
-            animator.SetBool("Idle", true);
+            if (horizontalAxis > 0)
+            {
+                animator.SetInteger("direction", 1);
+                animator.SetBool("idle", false);
+            }
+            else if (horizontalAxis < 0)
+            {
+                animator.SetInteger("direction", 3);
+                animator.SetBool("idle", false);
+            }
+            else
+            {
+                animator.SetBool("Idle", true);
+            }
         }
+        
+        
 
         Vector3 toMove = new Vector3(horizontalAxis,verticalAxis);
 		toMove.Normalize ();
