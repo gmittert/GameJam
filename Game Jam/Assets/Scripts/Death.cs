12a3e50d2386a27448bd517860bd7225939dc0b9
Neil Redman <nredman@uwaterloo.ca>
2015-05-30 21:07:24 -0400

    Some script changes, added death script

---
+++ Game Jam/Assets/Scripts/Death.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void Die(int a){
+		Debug.Log ("I'm Dead!");
+		Transform parent = transform.parent;
+		parent.SendMessage ("Die",1);
+	}
+}
