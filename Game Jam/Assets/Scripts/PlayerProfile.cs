07fc6203b1787a3e6f5f50ef45a2a6e8ae227003
Ian Holstead <ian.holstead@gmail.com>
2016-02-02 11:54:32 -0500

    Transition to XInput in progress

--- Game Jam/Assets/Scripts/PlayerProfile.cs
+++ Game Jam/Assets/Scripts/PlayerProfile.cs
@@ -1,9 +1,59 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using XInputDotNetPure;
+
+public enum Controls
+{
+    shoot,
+    light,
+    pause,
+    select,
+    back
+}
 
 public class PlayerProfile {
     string name;
+    Dictionary<Controls, List<GamePadButtons>> inputMapping;
 
+    
+
+    public PlayerProfile()
+    {
+        foreach (Controls control in System.Enum.GetValues(typeof(Controls)))
+        {
+            inputMapping[control] = new List<GamePadButtons>();
+        }
+    }
+
+    public bool setControl(Controls control)
+    {
+        string input = Input.inputString;
+        if(input != null)
+        {
+            //inputMapping[control].Add(input);
+            return true;
+        }
+        return false;
+    }
+
+    private static Dictionary<Controls, List<string>> getDefaultMapping()
+    {
+        Dictionary<Controls, List<string>> mapping = new Dictionary<Controls, List<string>>();
+        
+        foreach (Controls control in System.Enum.GetValues(typeof(Controls)))
+        {
+            mapping[control] = new List<string>();
+        }
+
+        mapping[Controls.shoot].Add("test");
+        mapping[Controls.light].Add("test");
+        mapping[Controls.pause].Add("test");
+        mapping[Controls.select].Add("test");
+        mapping[Controls.back].Add("test");
+
+        return mapping;
+    }
+    
 
 	
 }
